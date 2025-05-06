7074e78eb6b8569afba916e69e5bcce6cddb9c2e
Zsolt Harmath <harmathzs@gmail.com>
2025-05-06 10:39:59 +0200

    Implement LoadFromCsv method

--- 1prog-java/RealEstate/src/Ad.java
+++ 1prog-java/RealEstate/src/Ad.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class Ad {
 	public Integer Area;
@@ -12,4 +19,29 @@ public class Ad {
 	public String LatLong;
 	public Integer Rooms;
 	public Seller Seller;
+
+	public Ad(String line) {
+
+	}
+
+	public static ArrayList<Ad> LoadFromCsv(String fileName) {
+		ArrayList<Ad> ads = new ArrayList<>();
+		StringBuilder csv = new StringBuilder();
+		try {
+			FileReader f = new FileReader(fileName);
+			int c;
+			while ((c = f.read()) != -1) {
+				csv.append((char) c);
+			}
+			f.close();
+
+			String[] lines = csv.toString().split("\n");
+			for (String line: lines) {
+				ads.add(new Ad(line));
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return ads;
+	}
 }
