723bd6662ade26d8b813dccc69c1c1bb0c64d5e6
Zsolt Harmath <harmathzs@gmail.com>
2025-05-06 10:19:02 +0200

    Add Ad properties

--- 1prog-java/RealEstate/src/Ad.java
+++ 1prog-java/RealEstate/src/Ad.java
@@ -1,2 +1,15 @@
+import java.time.LocalDateTime;
+
 public class Ad {
+	public Integer Area;
+	public Category Category;
+	public LocalDateTime CreateAt;
+	public String Description;
+	public Integer Floors;
+	public Boolean FreeOfCharge;
+	public Integer Id;
+	public String ImageUrl;
+	public String LatLong;
+	public Integer Rooms;
+	public Seller Seller;
 }
