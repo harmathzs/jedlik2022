3edc6670ce9b762770793ef84286066836edc615
Zsolt Harmath <harmathzs@gmail.com>
2025-05-08 12:31:14 +0200

    Add Junit dependency

--- 1prog-java/RealEstateGUI/src/main/java/com/example/realestategui/TestApplication.java
+++ 1prog-java/RealEstateGUI/src/main/java/com/example/realestategui/TestApplication.java
@@ -1,4 +1,11 @@
 package com.example.realestategui;
 
+import org.junit.Test;
+
 public class TestApplication {
+	@Test
+	public void testApp() {
+		HelloController controller = new HelloController();
+		controller.onHelloButtonClick();
+	}
 }
