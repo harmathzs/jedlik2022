1b765fe1a16a70f13ab95c99bfdcf480709315d2
Zsolt Harmath <harmathzs@gmail.com>
2025-05-12 15:48:23 +0200

    Add /api/ingatlan endpoint

--- 3express/Harmath_Zsolt_backend/app.js
+++ 3express/Harmath_Zsolt_backend/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ingatlan'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database.');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/ingatlan', (req, res) => {
+    connection.query('SELECT ingatlanok.id, kategoriak.id, kategoriak.nev, leiras, hirdetesDatuma, tehermentes, ar, kepUrl FROM ingatlanok INNER JOIN kategoriak ON ingatlanok.kategoria=kategoriak.id', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Database error' });
+      return;
+    }
+    res.status(200).json(results);
+  });
 });
 
 app.listen(port, () => {
