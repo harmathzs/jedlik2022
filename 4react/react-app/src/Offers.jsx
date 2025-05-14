b3512059038c6b4de76c6d18273c70e59cf8f2d7
Zsolt Harmath <harmathzs@gmail.com>
2025-05-14 20:15:19 +0200

    Add Offers component

---
+++ 4react/react-app/src/Offers.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+import '../../Bootstrap UI/bootstrap.min.css';
+import './App.css';
+
+export default function Offers() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+
+    }, []);
+
+    return (
+    <div className="container mt-4">
+      <h2>Ingatlanok listája</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Cím</th>
+            <th>Típus</th>
+            <th>Ár</th>
+            <th>Méret</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </Table>
+    </div>
+  );
+
+  /* 
+            {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.cim}</td>
+              <td>{item.tipus}</td>
+              <td>{item.ar} Ft</td>
+              <td>{item.m2} m²</td>
+            </tr>
+          ))}
+
+  */
+}
