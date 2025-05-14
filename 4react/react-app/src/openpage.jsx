c8a6bc6ab46c5a8f96f655ff0350d848c28bc29b
Zsolt Harmath <harmathzs@gmail.com>
2025-05-14 15:35:16 +0200

    Add Openpage

---
+++ 4react/react-app/src/openpage.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import '../../Bootstrap UI/bootstrap.min.css';
+import './App.css';
+
+function Openpage() {
+    return(
+        <div className="container">
+            <div className="start w-100">
+                <h1 className="text-center pt-2 pt-lg-4">Á.L.B. Ingatlanügynöség</h1>
+                <div className="row">
+                    <div className="col-12 col-sm-6 text-center">
+                        <a className="btn btn-primary" href="#">Nézze meg kínálatunkat!</a>
+                    </div>
+                    <div className="col-12 col-sm-6 text-center">
+                        <a className="btn btn-primary" href="#">Hirdessen nálunk!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Openpage;
