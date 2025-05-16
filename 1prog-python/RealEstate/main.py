0265beb0c9078733437dd75f4b246deb1563caed
Zsolt Harmath <harmathzs@gmail.com>
2025-05-16 12:24:18 +0200

    Define classes

--- 1prog-python/RealEstate/main.py
+++ 1prog-python/RealEstate/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Category:
+	Id: int
+	Name: str
+
+class Seller:
+	Id: int
+	Name: str
+	Phone: str
+
+class Ad:
+	Area: int
+	Category: Category
+	CreateAt: datetime
+	Description: str
+	Floors: int
+	FreeOfCharge: bool
+	Id: int
+	ImageUrl: str
+	LatLong: str
+	Rooms: int
+	Seller: Seller
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
