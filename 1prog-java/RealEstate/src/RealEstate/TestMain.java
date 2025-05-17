f4689ead70c66a31f5c2a0622a40c40408b1b66b
Zsolt Harmath <harmathzs@gmail.com>
2025-05-17 14:16:09 +0200

    Create test methods

--- 1prog-java/RealEstate/src/RealEstate/TestMain.java
+++ 1prog-java/RealEstate/src/RealEstate/TestMain.java
@@ -7,4 +7,19 @@ public class TestMain {
 	public void testMain() {
 		Main.main(null);
 	}
+
+	@Test
+	public void testSeller() {
+		Seller seller = new Seller();
+		seller.Id = 1;
+		seller.Name = "Test";
+		seller.Phone = "+36701234567";
+	}
+
+	@Test
+	public void testCategory() {
+		Category category = new Category();
+		category.Id = 1;
+		category.Name = "test";
+	}
 }
