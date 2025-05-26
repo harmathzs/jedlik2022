3ba0e7b489635eb524e5c36ac37aae8adb68a9bb
Zsolt Harmath <harmathzs@gmail.com>
2025-05-26 14:55:18 +0200

    Outsource fetchData logic from NewAd

--- 4react/react-app/src/App.jsx
+++ 4react/react-app/src/App.jsx
@@ -6,8 +6,6 @@ import NewAd from './NewAd';
 import './App.css';
 
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path="/" element={<Openpage />} />
